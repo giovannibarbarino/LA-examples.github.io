022551f03299a5c8ea70a7b6b5cf7385e3705837
giovannibarbarino <139749758+giovannibarbarino@users.noreply.github.com>
2025-06-16 23:28:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const database = [
+  { id: 1, nome: "Oggetto A", tags: ["html", "css"] },
+  { id: 2, nome: "Oggetto B", tags: ["javascript", "css"] },
+  { id: 3, nome: "Oggetto C", tags: ["python", "ai"] }
+];
